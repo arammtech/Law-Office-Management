82b326daeca47c96fc38c575ddaa429940061d98
Abdulaziz <bdalzyzalbrnawy47@gmail.com>
2025-09-01 09:00:10 +0300

    feat: login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/auth/login/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "login", component:Login}
+];
---
+++ src/app/pages/auth/login/login/login.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Route } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+  constructor() {
+
+  }
+  login() {
+
+  }
+}
