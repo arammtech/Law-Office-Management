ffeda2ac978de02cb6e3b6a23f550588dc02468a
Abdulaziz <bdalzyzalbrnawy47@gmail.com>
2025-09-17 14:55:17 +0300

    feat(employees): implemnt employees list

--- src/app/pages/employee/mange-employee-page/mange-employee-page/mange-employee-page.ts
+++ src/app/pages/employee/mange-employee-page/mange-employee-page/mange-employee-page.ts
@@ -2,17 +2,51 @@ import { Component } from '@angular/core';
 import { PageHeaderComponent } from '../../../../../shared/components/page header/page-header-component/page-header-component';
 import { MatDialog } from '@angular/material/dialog';
 import { AddEmployeeComponent } from '../../components/add-employee/add-employee-component/add-employee-component';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-mange-employee-page',
-  imports: [PageHeaderComponent],
+  imports: [PageHeaderComponent, MatTableModule],
   templateUrl: './mange-employee-page.html',
   styleUrl: './mange-employee-page.css',
 })
 export class MangeEmployeePage {
-  /**
-   *
-   */
+  employees: IEmployeeRow[] = [
+    {
+      id: '1',
+      name: 'عبدالعزيز حسن',
+      role: 'مدير عام',
+      natId: '2234048409',
+      countryCode: 'نيجيريا',
+      email: 'bdalzyzalbrnawy47@gmail.com',
+    },
+    {
+      id: '1',
+      name: 'عبدالعزيز حسن',
+      role: 'مدير عام',
+      natId: '2234048409',
+      countryCode: 'نيجيريا',
+      email: 'bdalzyzalbrnawy47@gmail.com',
+    },
+    {
+      id: '1',
+      name: 'عبدالعزيز حسن',
+      role: 'مدير عام',
+      natId: '2234048409',
+      countryCode: 'نيجيريا',
+      email: 'bdalzyzalbrnawy47@gmail.com',
+    },
+  ];
+
+  displayedColumns: string[] = [
+    'positon',
+    'name',
+    'natId',
+    'email',
+    'role',
+    'countryName',
+    'actions'
+  ];
   constructor(private dialogof: MatDialog) {}
 
   addEmployee() {
@@ -21,4 +55,13 @@ export class MangeEmployeePage {
       minWidth: '400px',
     });
   }
+}
+
+export interface IEmployeeRow {
+  id: string;
+  name: string;
+  natId: string;
+  countryCode: string;
+  role: string;
+  email: string;
 }
