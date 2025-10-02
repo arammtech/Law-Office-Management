6b08b0ca3c8c5ef45af4ba1ced508877e5785bee
Abdulaziz <bdalzyzalbrnawy47@gmail.com>
2025-10-02 04:14:09 +0300

    add credential to login

--- src/core/services/auth/auth-management.ts
+++ src/core/services/auth/auth-management.ts
@@ -33,7 +33,7 @@ export class AuthManagement {
           password: password,
         },
         {
-          // withCredentials: true,
+          withCredentials: true,
         }
       )
       .pipe(
